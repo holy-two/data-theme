4a72bea67f9dc41ec912d21ebccdbea5a2a8b2c2
Jimmy <jimmyrss1102@gmail.com>
2024-08-03 02:14:05 +0800

    refactor: use `typescript`

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ Object.defineProperty(window, "theme", {
   get: () =>
     localStorage.getItem("theme") ??
     ["dark", "light"][
-      +!window.matchMedia?.("(prefers-color-scheme: dark)")?.matches
+    +!window.matchMedia?.("(prefers-color-scheme: dark)")?.matches
     ],
-})
+});
+
 document.documentElement.dataset["theme"] = window.theme;
+
+export { };
+declare global {
+  interface Window {
+    theme: string;
+  }
+}
