7c61988bc92ca5eeafcd992b855437263b273f42
Jimmy <jimmyrss1102@gmail.com>
2024-08-02 13:25:15 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+Object.defineProperty(window, "theme", {
+  get: () =>
+    localStorage.getItem("theme") ??
+    ["dark", "light"][
+      +!window.matchMedia?.("(prefers-color-scheme: dark)")?.matches
+    ],
+})
+document.documentElement.dataset["theme"] = window.theme;
