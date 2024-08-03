e3ddca7489da0c57c1296cb5df1bce40e6bb2c6d
Jimmy <jimmyrss1102@gmail.com>
2024-08-03 15:54:12 +0800

    chore: add `vite` template

---
+++ example/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <button id="theme-status" @click="changeTheme">
+    切换Theme
+  </button>
+</template>
+
+<script setup lang="ts">
+const themeTuple = ["dark", "light"];
+
+const changeTheme = () => {
+  const theme = themeTuple.at(themeTuple.indexOf(window.theme) - 1);
+  document.documentElement.dataset["theme"] = theme;
+  localStorage.setItem("theme", theme);
+
+  const themeStatusElement = document.getElementById("theme-status");
+  if (themeStatusElement) {
+    themeStatusElement.innerText = `当前主题: ${window.theme}`;
+  }
+  console.log(theme);
+};
+
+</script>
+
+<style>
+:root[data-theme=dark] {
+  color-scheme: dark;
+}
+</style>
