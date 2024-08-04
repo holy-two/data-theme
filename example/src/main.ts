685e42a43ab51af56d286c78d4bdb1ad90f17dd3
☘ <kogawananari@gmail.com>
2024-08-05 01:15:27 +0800

    :construction: test for vite

--- example/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '../../dist/index.global.js'
-
-createApp(App).mount('#app')
---
+++ example/vite-project/src/main.ts
@@ -0,0 +1,6 @@
+import "./style.css"
+import { toggle } from "../../../dist/"
+
+const button = document.querySelector("button")
+
+button?.addEventListener?.("click", toggle)
