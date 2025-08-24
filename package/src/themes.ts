804d46c8614c812a748a8d518c0085a04a8f5660
阿菜 Cai <jimmyrss1102@gmail.com>
2025-08-24 16:14:31 +0800

    :sparkles: toggle拓展

--- package/src/themes.ts
+++ package/src/themes.ts
@@ -2,6 +2,14 @@ const themes = ["dark", "light"] as const
 
 export type Theme = (typeof themes)[number]
 
+export function isTheme(theme?: any): theme is Theme {
+  return themes.includes(theme as Theme)
+}
+
+export function isEvent(event?: any): event is CustomEvent<Theme> {
+  return isTheme(((event ?? {}) as CustomEvent)?.detail)
+}
+
 declare global {
   interface Window {
     readonly theme: Theme
--- package/src/toggle.ts
+++ package/src/toggle.ts
@@ -1,9 +1,17 @@
+import { Theme, isTheme, isEvent } from "./themes"
 import themes from "./themes"
 
-export default function () {
-  const theme = themes.at(themes.indexOf(window.theme) - 1)
-  if (theme) {
-    document.documentElement.dataset["theme"] = theme
-    localStorage.setItem("theme", theme)
+export default function (
+  theme?: Theme | CustomEvent<Theme> | Event,
+  root: HTMLElement = document.documentElement
+) {
+  if (!isTheme(theme)) {
+    if (isEvent(theme)) {
+      theme = theme.detail
+    } else {
+      theme = themes.at(themes.indexOf(window.theme) - 1) as Theme
+    }
   }
+  root.dataset["theme"] = theme
+  localStorage.setItem("theme", theme)
 }
